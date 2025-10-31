bd0053d39f3639f02421aa7d958ea19eb04e4a42
sean <yoshinryu.sean@gmail.com>
2025-10-31 20:55:20 +0800

    FINAL FIX: Moved 'use client' to absolute Line 1 in the Firebase provider file.

--- src/firebase/provider.tsx
+++ src/firebase/provider.tsx
@@ -1,5 +1,5 @@
+'use client'; 
 
-'use client';
 
 import React, { DependencyList, createContext, useContext, ReactNode, useMemo, useState, useEffect } from 'react';
 import { FirebaseApp } from 'firebase/app';
